d2382df6f6173b3adb1dc928c92804bd00a39a1d
Ashley Davis <ashley.davis@realseriousgames.com>
2016-03-09 16:57:43 +1000

    Very simple NodeJS test app for deployment to Azure.

---
+++ server.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var staticPath = path.join(__dirname, '../Client');
+console.log(staticPath);
+app.use(express.static(staticPath));
+
+process.on('uncaughtException', function (err) {
+    console.error('Uncaught Exception: ' + err.message + '\r\n' + err.stack);
+});
+
+app.get('/is-alive', function (req, res) {
+	res.status(200).end();
+});
+
+app.get('/example-rest-api', function (req, res) {
+	res.json({ hello: 'computer'});
+});
+
+var server = app.listen(3000, function () {
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
