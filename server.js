a901205524b8bf257332278ceb856c73a67247f6
Ashley Davis <ashley.davis@realseriousgames.com>
2016-03-10 14:19:35 +1000

    Updated the port so it can be specified in the environment.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/example-rest-api', function (req, res) {
 	res.json({ hello: 'computer'});
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
 	var host = server.address().address
 	var port = server.address().port
