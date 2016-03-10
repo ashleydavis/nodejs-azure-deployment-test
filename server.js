42b52e7c468e3a7556f06b0362636a256dd93f2d
Ashley Davis <ashley.davis@realseriousgames.com>
2016-03-10 14:30:37 +1000

    Removed an unnecessary example.

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ process.on('uncaughtException', function (err) {
     console.error('Uncaught Exception: ' + err.message + '\r\n' + err.stack);
 });
 
-app.get('/is-alive', function (req, res) {
-	res.status(200).end();
-});
-
 app.get('/example-rest-api', function (req, res) {
 	res.json({ hello: 'computer'});
 });
